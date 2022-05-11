d3e7f28600aa32260278b4769b80436b7e242bf7
Antonina Nikolaeva <minimalanny@gmail.com>
2022-05-12 00:50:51 +0500

    тест

---
+++ git/git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seminar
+{
+
+    internal class MyProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my first application.");
+            Console.ReadLine();
+        }
+    }
+}
